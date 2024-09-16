f58d0114ace80bb2f501f93f2f5bd61ee865c48d
Nikolakopoulos Filippos <f.nikolakopoulos@gmail.com>
2024-09-16 23:00:13 +0300

    Examples

---
+++ Examples/AddLine.py
@@ -0,0 +1,17 @@
+
+
+
+import fdata
+import acad
+from acad.acad import MODEL
+
+#from acad.document import document
+
+
+#d0 = bcad.DOC()
+m0 = MODEL()
+
+pt0 = "0,0"
+pt1 = "1,1"
+
+myline= m0.AddLine(pt0,pt1)
---
+++ Examples/copy_objects.py
@@ -0,0 +1,18 @@
+
+
+
+import fdata
+import acad.acad as bcad
+
+from acad.document import document
+
+d1 = document()
+
+template = "C:\\Users\\filip\\AppData\\Local\\Bricsys\\BricsCAD\\V24x64\\en_US\\Templates\\Default-mm.dwt"
+
+d0 = bcad.DOC()
+#mydoc = d0.new(template)
+
+
+
+
