b89288f952b58db8bbc87aa4738d9e1ef30b1661
Nikolakopoulos Filippos <f.nikolakopoulos@gmail.com>
2025-02-17 01:36:18 +0200

    works with autospool

---
+++ acad/plot.py
@@ -0,0 +1,44 @@
+#from acad.acad import DOC
+
+d=DOC()
+
+
+output_pdf = "C://Users//filip//AppData//Roaming//draw//plots//0.pdf"
+
+
+doc = d.doc
+# Get the Active Layout (Model or Paper Space)
+layout = doc.Layouts.Item("Model")  # Change to a paper space layout if needed
+
+
+PlotDevice = "Print As PDF.pc3"
+Orientation = 'Landscape'
+
+#works if style is autospool
+# Use the command-line PLOT method
+plot_command = (
+    '-PLOT\n' +  # Silent plot mode
+    'Y\n' +  # Detailed plot configuration
+    'Model\n' +  # Layout (Change to "Layout1" if needed)
+    PlotDevice + '\n' + # Plotter name
+    'A4\n' +  # Paper size
+    '\n' + 
+    Orientation + '\n' + # Orientation
+    'No\n' +  # Plot upside down?
+    'Extents\n' +  # Plot area
+    'Fit' + '\n' + 
+    'Center\n' +  # center
+ 
+    'Yes\n' +  # Center the plot
+    'default.ctb' + '\n' +  # plot style name
+    'Yes' + '\n' + # plot with lineweights
+    'A' + '\n' + # shade plot
+    'Yes'
+#    f'"{output_pdf}"\n'   # Output file path
+)
+
+
+
+doc.SendCommand(plot_command)
+
+
