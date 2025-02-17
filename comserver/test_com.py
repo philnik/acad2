fedaa6359ea234154b059a1e80456ba0afb3b438
Nikolakopoulos Filippos <f.nikolakopoulos@dimtech.gr>
2025-02-17 14:19:56 +0200

    does not connect

---
+++ comserver/test_com.py
@@ -0,0 +1,10 @@
+
+
+
+import win32com.client
+import pythoncom
+from win32com.client import Dispatch, VARIANT
+from pythoncom import VT_VARIANT
+import sys
+
+ac = win32com.client.Dispatch("MyPythonCOM.Server")
