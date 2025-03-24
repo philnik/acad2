bd3d80de3625fd4abaacb42bf617bd91e3d63ceb
Nikolakopoulos Filippos <f.nikolakopoulos@dimtech.gr>
2025-03-24 11:05:03 +0200

    works ok

---
+++ scripts/rename_prn2.py
@@ -0,0 +1,13 @@
+
+import sys
+
+sys.path.append("c:/Users/f.nikolakopoulos/AppDATA/Roaming/source/acad2/acad/")
+
+import numpy
+import sqlite3
+import pandas
+import jinja2
+from rename import prn2pdf
+
+
+prn2pdf()
