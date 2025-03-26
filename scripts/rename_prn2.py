3ba0bcc52b5f62008a03df77aac290f100f1a223
Nikolakopoulos Filippos <f.nikolakopoulos@dimtech.gr>
2025-03-26 12:45:51 +0200

    add argument fetching

--- scripts/rename_prn2.py
+++ scripts/rename_prn2.py
@@ -8,6 +8,20 @@ from rename import (prn2pdf,
 
 from backup_file import backup_current_document
 
+
+def fetch_args():
+    res = []
+    for i in range(1,5):
+        try:
+            fl = float(sys.argv[i])
+            res.append(fl)
+            print(f"""fl[{i}]={fl}""")
+        except ValueError:
+            print("Error: No more floats")
+    return res
+
+
+
 prn2pdf()
 backup_current_document()
 clean_plot_folder()
