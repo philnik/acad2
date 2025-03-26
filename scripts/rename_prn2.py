6dfb8d8b9fb8c0d79d2debaf1d8e3f0b7d15582e
nif <f.nikolakopoulos@dimtech.gr>
2025-03-26 12:24:12 +0200

    working on storing blocks

--- scripts/rename_prn2.py
+++ scripts/rename_prn2.py
@@ -6,6 +6,8 @@ sys.path.append("c:/Users/f.nikolakopoulos/AppDATA/Roaming/source/acad2/acad/")
 from rename import (prn2pdf,
                     clean_plot_folder)
 
+from backup_file import backup_current_document
 
 prn2pdf()
+backup_current_document()
 clean_plot_folder()
