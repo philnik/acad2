d89452eaaba51d9039ef3e7ad0ea1ed207cde153
Nikolakopoulos Filippos <f.nikolakopoulos@gmail.com>
2025-03-29 12:07:49 +0200

    done

--- comserver/test_comserver4.py
+++ comserver/test_comserver4.py
@@ -2,7 +2,9 @@ import win32com.client
 
 #  runas /user:Adminstrator regsvr32 /u C:/Users/filip/source/repos/test_com/bin/Debug/test_com.dll
 # runas /user:Adminstrator C:/Windows/Microsoft.NET/Framework/v4.0.30319/RegAsm.exe  C:/Users/filip/source/repos/test_com/bin/Debug/test_com.dll /codebase /tlb
-# runas /user:Adminstrator "C:/Windows/Microsoft.NET/Framework/v4.0.30319/RegAsm.exe" c:/Users/filip/AppData/Roaming/python/acad2/comserver/bin/Debug/test_com.dll /codebase /tlb
+# runas /user:Adminstrator C:/Windows/Microsoft.NET/Framework/v4.0.30319/RegAsm.exe  /u C:/Users/filip/source/repos/test_com/bin/Debug/test_com.dll
+# runas /user:Adminstrator C:/Windows/Microsoft.NET/Framework/v4.0.30319/RegAsm.exe c:/Users/filip/AppData/Roaming/python/acad2/comserver/test_com/bin/Debug/test_com.dll /codebase /tlb
+# runas /user:Adminstrator C:/Windows/Microsoft.NET/Framework/v4.0.30319/RegAsm.exe /u c:/Users/filip/AppData/Roaming/python/acad2/comserver/bin/Debug/test_com.dll
 #Set obj = CreateObject("MyCOML")
 #MsgBox obj.SayHello("John")
 #Set calculator = CreateObject("SimpleComServer.Calculator")
